ab1b29f141f81ae340bace30c59209d678ceb2f7
Sangraiz khan <87944923+HEALER-PARK-BONG-SU@users.noreply.github.com>
2022-07-13 10:54:01 +0500

    Added a for each function to empty js file

---
+++ app.js
@@ -0,0 +1,11 @@
+const persons = ['imran','kashif','jawad','sangraiz','hamza','maaz'];
+persons.foreach(function(person){
+  console.log('Hello',person);
+});
+
+/*const greeting = function(person){
+  console.log('Hello',person);
+}
+
+persons.foreach(greeting);
+*/
